22e2fc25a6f11bb6dca72080c7ce3e9f4ecc6434
brian behling <brian.behling@gmail.com>
2023-12-04 12:22:07 -0700

    Added processed text

--- pages/channels.tsx
+++ pages/channels.tsx
@@ -4,19 +4,21 @@ import styles from "./channels.module.css";
 export default function Channels({ channels }) {
   return (
     <div>
-      <h1>Top 20 Movies of All Time</h1>
-      <p>
-        <small>(According to Metacritic)</small>
-      </p>
+      <h1>You Tube Channels</h1>
       <ul>
         {channels.map((channel) => (
           <li>
             <h2>{channel.title}</h2>
             {channel.videos.map((video) => (
               <ul>
-                <li>
-                  <p className={styles.p}>{video.Description}</p>
-                </li>
+                {video.DescriptionRaw.length > 0 && (
+                  <div>
+                    <h3>Raw Description</h3>
+                    <p className={styles.p}>{video.DescriptionRaw}</p>
+                    <h3>Processed Description</h3>
+                    <p className={styles.p}>{video.ProcessedText}</p>
+                  </div>
+                )}
               </ul>
             ))}
           </li>
