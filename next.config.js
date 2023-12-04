4e00ade5eb46105fe261ce4cc7302944ccf5ad16
brian behling <brian.behling@gmail.com>
2023-12-04 13:09:11 -0700

    Redirects

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   output: "standalone",
+
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/channels",
+        permanent: true,
+      },
+    ];
+  },
 };
