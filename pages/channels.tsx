99500a8a98a257dacac6d1cc5287988e0ed65f56
brian behling <brian.behling@gmail.com>
2023-12-19 21:46:59 -0700

    Added autolink

--- pages/channels.tsx
+++ pages/channels.tsx
@@ -3,27 +3,73 @@ import styles from "./channels.module.css";
 
 export default function Channels({ channels }) {
   return (
-    <div>
-      <h1>You Tube Channels</h1>
-      <ul>
-        {channels.map((channel) => (
-          <li>
-            <h2>{channel.title}</h2>
-            {channel.videos.map((video) => (
-              <ul>
-                {video.DescriptionRaw.length > 0 && (
-                  <div>
-                    <h3>Raw Description</h3>
-                    <p className={styles.p}>{video.DescriptionRaw}</p>
-                    <h3>Processed Description</h3>
-                    <p className={styles.p}>{video.ProcessedText}</p>
-                  </div>
-                )}
-              </ul>
+    <div className="container">
+      <div className="row">
+        <div className="col-10">
+          <div>
+            {channels.map((channel) => (
+              <div className="card mb-3">
+                <div className="card-body">
+                  <h2 className="card-title">{channel.title}</h2>
+                  {channel.videos.map((video, i) => (
+                    <div>
+                      {video.DescriptionRaw.length > 0 && (
+                        <div className="mb-1">
+                          <h4 className="card-title">{`Video Title: ${video.VideoTitle}`}</h4>
+                          <div className="accordion" id="accordionExample">
+                            <div className="accordion-item">
+                              <h2 className="accordion-header">
+                                <button
+                                  className="accordion-button collapsed"
+                                  type="button"
+                                  data-bs-toggle="collapse"
+                                  data-bs-target={`#raw${i}${channel.channelId}`}
+                                >
+                                  <h4>Raw Description</h4>
+                                </button>
+                              </h2>
+                              <div
+                                id={`raw${i}${channel.channelId}`}
+                                className="accordion-collapse collapse"
+                                data-bs-parent="#accordionExample"
+                              >
+                                <div className="accordion-body">
+                                  <div className={styles.p}>{video.DescriptionRaw}</div>
+                                </div>
+                              </div>
+                            </div>
+                            <div className="accordion-item">
+                              <h2 className="accordion-header">
+                                <button
+                                  className="accordion-button collapsed"
+                                  type="button"
+                                  data-bs-toggle="collapse"
+                                  data-bs-target={`#ai${i}${channel.channelId}`}
+                                >
+                                  <h4>Processed Description</h4>
+                                </button>
+                              </h2>
+                              <div
+                                id={`ai${i}${channel.channelId}`}
+                                className="accordion-collapse collapse"
+                                data-bs-parent="#accordionExample"
+                              >
+                                <div className="accordion-body">
+                                  <p className={styles.p}>{video.ProcessedText}</p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      )}
+                    </div>
+                  ))}
+                </div>
+              </div>
             ))}
-          </li>
-        ))}
-      </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import clientPromise from "../lib/mongodb";
 import styles from "./channels.module.css";
-import SideMenu from "../components/sideMenu";
+import AutoLink from "../components/autolink";
 
 export default function Home({ channels }) {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-2"></div>
         <div className="col-10">
           <h1>You Tube Channels</h1>
           <div>
@@ -26,27 +25,6 @@ export default function Home({ channels }) {
                                   className="accordion-button collapsed"
                                   type="button"
                                   data-bs-toggle="collapse"
-                                  data-bs-target={`#raw${i}${channel.channelId}`}
-                                >
-                                  <h4>Raw Description</h4>
-                                </button>
-                              </h2>
-                              <div
-                                id={`raw${i}${channel.channelId}`}
-                                className="accordion-collapse collapse"
-                                data-bs-parent="#accordionExample"
-                              >
-                                <div className="accordion-body">
-                                  <div className={styles.p}>{video.DescriptionRaw}</div>
-                                </div>
-                              </div>
-                            </div>
-                            <div className="accordion-item">
-                              <h2 className="accordion-header">
-                                <button
-                                  className="accordion-button collapsed"
-                                  type="button"
-                                  data-bs-toggle="collapse"
                                   data-bs-target={`#ai${i}${channel.channelId}`}
                                 >
                                   <h4>Processed Description</h4>
@@ -58,7 +36,9 @@ export default function Home({ channels }) {
                                 data-bs-parent="#accordionExample"
                               >
                                 <div className="accordion-body">
-                                  <p className={styles.p}>{video.ProcessedText}</p>
+                                  <p className={styles.p}>
+                                    <AutoLink text={video.ProcessedText} />{" "}
+                                  </p>
                                 </div>
                               </div>
                             </div>
