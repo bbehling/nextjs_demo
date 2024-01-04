f78cc95be5c31bcf2c33b198840dfe98bc73b733
brian behling <brian.behling@gmail.com>
2024-01-04 14:41:19 -0700

    Added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import type { AppProps } from "next/app";
 import "../styles/scss/global.scss";
 import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
+import { useRouter } from "next/router";
 
 //font awesome stuff
 import { config } from "@fortawesome/fontawesome-svg-core";
@@ -10,20 +12,34 @@ import Script from "next/script";
 config.autoAddCss = false;
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   useEffect(() => {
     import("bootstrap/dist/js/bootstrap");
-  }, []);
+
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return (
     <>
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`} />
       <Script
-        id="Adsense-id"
-        async
-        strategy="beforeInteractive"
-        onError={(e) => {
-          console.error("Script failed to load", e);
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
         }}
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5962139115772386"
       />
       <Component {...pageProps} />
       <Layout>
